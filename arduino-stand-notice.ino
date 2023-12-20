ff5ca71837aa465b5e94432faeff2101b9baa393
Peter Ramsing <peter.ramsing@synaptiq.ai>
2023-12-20 13:33:28 -0800

    Updates with latest code

--- arduino-stand-notice.ino
+++ arduino-stand-notice.ino
@@ -1,40 +1,23 @@
-// const int buttonPin = 4;
-// const int greenLedPin = 2;
-// const int redLedPin = 3;
-// int buttonState = 0;
-
-// void setup() {
-//   pinMode(greenLedPin, OUTPUT);
-//   pinMode(redLedPin, OUTPUT);
-//   pinMode(buttonPin, INPUT);
-// }
-
-// void loop() {
-//   buttonState = digitalRead(buttonPin);
-//   if (buttonState == HIGH) {
-//     digitalWrite(greenLedPin, HIGH);
-//     digitalWrite(redLedPin, LOW);
-//     delay(1500000); // Wait for 25 minutes
-//     digitalWrite(greenLedPin, LOW);
-//     digitalWrite(redLedPin, HIGH);
-//   }
-// }
-
-const int greenLedPin = 2;
 const int buttonPin = 4;
+const int greenLedPin = 2;
+const int redLedPin = 3;
+int buttonState = 0;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
   pinMode(greenLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
-    // Button is pressed
     digitalWrite(greenLedPin, HIGH);
-  } else {
-    // Button is not pressed
+    digitalWrite(redLedPin, LOW);
+    delay(1500000); // Wait for 25 minutes
+    // delay(5000); // Demo time of 5 seconds
     digitalWrite(greenLedPin, LOW);
+    digitalWrite(redLedPin, HIGH);
   }
 }
+
